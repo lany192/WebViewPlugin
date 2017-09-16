541de0b2f69c55efc28dbf17df9e5e87a6678e7f
Lany <lany192@qq.com>
2017-09-16 10:16:14 +0800

    update

--- app/src/main/java/com/github/lany192/plugin/webview/WebActivity.java
+++ app/src/main/java/com/github/lany192/plugin/webview/WebActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.qihoo360.replugin.loader.a.PluginFragmentActivity;
 
@@ -13,6 +17,8 @@ public class WebActivity extends PluginFragmentActivity {
     private static final String key_title = "key_title";
     private String mUrl;
     private String mTitle;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static void start(Context ctx, String title, String url) {
         Intent intent = new Intent(ctx, WebActivity.class);
@@ -32,6 +38,34 @@ public class WebActivity extends PluginFragmentActivity {
         } else {
             Log.e(TAG, "onCreate:请检查传单参数");
             finish();
+        }
+        mWebView = (WebView) findViewById(R.id.webview2);
+        mProgressBar = (ProgressBar) findViewById(R.id.web_progressbar);
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+        mWebView.setWebViewClient(mWebViewClient);
+        mWebView.loadUrl(mUrl);
+    }
+
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    };
+
+    public class MyWebChromeClient extends android.webkit.WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgressBar.setVisibility(View.GONE);
+            } else {
+                if (mProgressBar.getVisibility() == View.GONE) {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+                mProgressBar.setProgress(newProgress);
+            }
+            super.onProgressChanged(view, newProgress);
         }
     }
 }
