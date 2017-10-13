2f3ed11d91d3945be9136182140c8adbee2c37e7
Lany <lany192@qq.com>
2017-10-13 21:36:07 +0800

    update

--- app/src/main/java/com/github/lany192/plugin/webview/WebActivity.java
+++ app/src/main/java/com/github/lany192/plugin/webview/WebActivity.java
@@ -1,71 +1,162 @@
 package com.github.lany192.plugin.webview;
 
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.qihoo360.replugin.loader.a.PluginAppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class WebActivity extends PluginAppCompatActivity {
-    protected final String TAG = "WebActivity";
-    private static final String key_url = "key_url";
-    private static final String key_title = "key_title";
+    private final String TAG = "WebViewPlugin";
+    @BindView(R.id.web_toolbar_title_text)
+    TextView mTitleText;
+    @BindView(R.id.web_toolbar_close_btn)
+    ImageButton mCloseBtn;
+    @BindView(R.id.web_view)
+    WebView mWebView;
+    @BindView(R.id.web_progressbar)
+    ProgressBar mProgressBar;
+
     private String mUrl;
     private String mTitle;
-    private WebView mWebView;
-    private ProgressBar mProgressBar;
-
-    public static void start(Context ctx, String title, String url) {
-        Intent intent = new Intent(ctx, WebActivity.class);
-        intent.putExtra(key_title, title);
-        intent.putExtra(key_url, url);
-        ctx.startActivity(intent);
-    }
+    private String mHeaders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        ButterKnife.bind(this);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
-            mUrl = bundle.getString(key_url);
-            mTitle = bundle.getString(key_title);
+            mUrl = bundle.getString("key_url");
+            mTitle = bundle.getString("key_title");
+            mHeaders = bundle.getString("key_headers");
         } else {
-            Log.e(TAG, "onCreate:请检查传单参数");
+            Log.e(TAG, "请检查传单参数");
             finish();
         }
-        mWebView = (WebView) findViewById(R.id.web_view);
-        mProgressBar = (ProgressBar) findViewById(R.id.web_progressbar);
         mWebView.setWebChromeClient(new MyWebChromeClient());
-        mWebView.setWebViewClient(mWebViewClient);
+        mWebView.clearCache(true);
+
+        final WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSavePassword(false);
+        webSettings.setLoadWithOverviewMode(true);// 自动适应手机屏幕
+        webSettings.setUseWideViewPort(true);
+        webSettings.setAppCacheEnabled(true);
+        if (Build.VERSION.SDK_INT >= 19) {
+            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+        if (TextUtils.isEmpty(mTitle)) {
+            mTitleText.setText("");
+        } else {
+            mTitleText.setText(mTitle);
+        }
+        mWebView.setWebViewClient(webViewClient);
         mWebView.loadUrl(mUrl);
     }
 
-    private WebViewClient mWebViewClient = new WebViewClient() {
+    private WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            mWebView.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            mWebView.loadUrl("about:blank");
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i(TAG, "url:" + url);
             view.loadUrl(url);
             return super.shouldOverrideUrlLoading(view, url);
         }
     };
 
-    public class MyWebChromeClient extends android.webkit.WebChromeClient {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWebView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+    }
+
+    @OnClick(R.id.web_toolbar_back_btn)
+    public void onBackClicked() {
+        onCustomBackPressed();
+    }
+
+    public class MyWebChromeClient extends WebChromeClient {
         @Override
         public void onProgressChanged(WebView view, int newProgress) {
-            if (newProgress == 100) {
-                mProgressBar.setVisibility(View.GONE);
-            } else {
-                if (mProgressBar.getVisibility() == View.GONE) {
-                    mProgressBar.setVisibility(View.VISIBLE);
+            if (mProgressBar != null) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    if (mProgressBar.getVisibility() == View.GONE)
+                        mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
                 }
-                mProgressBar.setProgress(newProgress);
             }
             super.onProgressChanged(view, newProgress);
         }
+    }
+
+    private void onCustomBackPressed() {
+        if (mWebView.canGoBack()) {
+            mCloseBtn.setVisibility(View.VISIBLE);
+            mWebView.goBack();
+            return;
+        }
+        onBackPressed();
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            onCustomBackPressed();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @OnClick(R.id.web_toolbar_close_btn)
+    public void onCloseClicked() {
+        finish();
     }
 }
